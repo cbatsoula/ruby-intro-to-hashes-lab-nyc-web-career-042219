e0f9786d708c2bd3e6d6d7acc9cc90ae03402042
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-13 02:43:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,6 @@ def monopoly_with_third_tier
     }
 	  }
 end
-
 def monopoly_with_fourth_tier
   monopoly =  {
     :railroads => {
@@ -64,6 +63,4 @@ def monopoly_with_fourth_tier
       }
     }
 	  }
-
-
 end
